6624195b2d08ee78b69ee0190eb4e2b929a9ef94
zlm19900414 <985746543@qq.com>
2019-12-05 17:15:01 +0900

    test

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -1,15 +1,15 @@
 package com.xuecheng.manage_cms.service;
 
+import antlr.StringUtils;
 import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 /**
@@ -33,20 +33,40 @@ public class PageService {
      */
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest){
 
+        if (queryPageRequest == null) {
+            queryPageRequest = new QueryPageRequest();
+        }
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().
+                withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.exact());
+
+        CmsPage cmsPage = new CmsPage();
+        if (StringUtil.isNullOrEmpty(queryPageRequest.getPageId())) {
+            cmsPage.setPageId(queryPageRequest.getPageId());
+        }
+        if(StringUtil.isNullOrEmpty(queryPageRequest.getPageAliase())){
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+        if(StringUtil.isNullOrEmpty(queryPageRequest.getSiteId())){
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        if(StringUtil.isNullOrEmpty(queryPageRequest.getTemplateId())){
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
         //分页参数
-        if(page <=0){
+        if (page <= 0) {
             page = 1;
         }
-        page = page -1;
-        if(size<=0){
+        page = page - 1;
+        if (size <= 0) {
             size = 10;
         }
-        Pageable pageable = PageRequest.of(page,size);
-        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        Example<CmsPage> example = Example.of(cmsPage,exampleMatcher);
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CmsPage> all = cmsPageRepository.findAll(example,pageable);
         QueryResult queryResult = new QueryResult();
         queryResult.setList(all.getContent());//数据列表
         queryResult.setTotal(all.getTotalElements());//数据总记录数
-        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
         return queryResponseResult;
     }
 }
